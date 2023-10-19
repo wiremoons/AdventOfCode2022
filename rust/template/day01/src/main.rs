0d5fb491a43ce968d55d966e1a8ee9c9f6eec277
Simon Rowe <simon@wiremoons.com>
2023-10-19 16:26:37 +0100

    Start of Rust structure for puzzles.

---
+++ rust/template/day01/src/main.rs
@@ -0,0 +1,26 @@
+// Advent of Code 2022
+//
+
+const DAY: &'static str = "Day 01";
+
+fn main() {
+    // set the location of the puzzle inut data files
+    let _puzzle_data = concat!(env!("CARGO_MANIFEST_DIR"), "/data/puzzle_input.txt");
+    let _test_data = concat!(env!("CARGO_MANIFEST_DIR"), "/data/test_input.txt");
+
+    println!("\nAdvent of Code 2022 Results for {}:", DAY);
+    println!("  Part 1: {}", part_one());
+    println!("  Part 2: {}", part_two());
+}
+
+// Solution for part one
+fn part_one() -> String {
+    //todo
+    return "TBC".to_string();
+}
+
+// Solution for part two
+fn part_two() -> String {
+    //todo
+    return "TBC".to_string();
+}
---
+++ rust/template/src/main.rs
@@ -0,0 +1,26 @@
+// Advent of Code 2022
+//
+
+const DAY: &'static str = "Day 01";
+
+fn main() {
+    // set the location of the puzzle inut data files
+    let puzzle_data = concat!(env!("CARGO_MANIFEST_DIR"), "/data/puzzle_input.txt");
+    let test_data = concat!(env!("CARGO_MANIFEST_DIR"), "/data/test_input.txt");
+
+    println!("Advent of Code 2022 Results for {}:\n", DAY);
+    println!("Part 1: {}\n", part_one());
+    println!("Part 1: {}\n", part_two());
+}
+
+// Solution for part one
+fn part_one() -> String {
+    //todo
+    return "TBC".to_string();
+}
+
+// Solution for part two
+fn part_two() -> String {
+    //todo
+    return "TBC".to_string();
+}
